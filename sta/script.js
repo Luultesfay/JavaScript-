f0ceb7bc167770347cb9ec2b5bffe78a90bdd3cb
luul negasi <luultesfay24@gmail.com>
2021-08-18 07:45:06 -0700

    code changed

--- sta/script.js
+++ sta/script.js
@@ -2,3 +2,8 @@ let js = "amaizing";
 //if (js === "amaizing") alert("coding js is Fun always with luul!");
 45 + 54 - 6;
 console.log(45 + 54 - 6);
+
+console.log("I am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+console.log(typeof ("23" > "18"));
